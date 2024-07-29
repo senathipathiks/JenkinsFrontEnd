6e0ea86f5757ad831cfc7ad15b5d0501ee49fbc8
Sena <senathipathik@gmail.com>
2024-07-29 12:34:38 +0530

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer'
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Test Suite for App',()=> {
+
+  
+  test("Snapshot test ", () => {
+    //render(<App />);
+    const comp = renderer.create(<App />)
+    const tree=comp.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  // it('renders welcome to Relevantz', () => {
+  //   render(<App />);
+  //   const linkElement = screen.getByText("Hi Welcome to Relevantz");
+  //   expect(linkElement).toBeInTheDocument();
+  // });
+})
+
